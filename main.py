5e64d737e5f081d1ad2ed039414507131465a202
Mamad <141618496+mamamamad@users.noreply.github.com>
2024-04-13 09:30:45 +0330

    modify

--- main.py
+++ main.py
@@ -1,30 +1,38 @@
 from selenium import webdriver
-
-
-
 import time
 
 
-username = "4001226047"
-password = "5720157913"
+class Login:
+    """ This class is for managing the entire login and logout process """
+    def __init__(self) -> None:
+        self.password = 0
+        self.username= 0
+
+    def log_in(self):
+        # login with selenium 
+        driver = webdriver.Chrome()
+        driver.get('https://internet.birjand.ac.ir/login')
+        driver.find_element('id',"username").send_keys(self.username)
+        driver.find_element('id',"password").send_keys(self.password)
+        driver.find_element("css selector", ".loginButton").click()
+        driver.quit()
+    def get_passuser(self):
+        self.username = input("please enter Username: ")
+        self.password = input("please enter Password: ")
+    def save_file(self):
+        # This function is for saving information in the file.
+        file = open("C:\Program Files\logis_bir\up.txt",'w') 
+        file.write(self.password)
+        file.write(self.username)  
+        file.write('a')  
+        file.write('b')  
+        file.close()
+          
 
 
-driver = webdriver.Chrome()
+l= Login
 
-driver.get('https://internet.birjand.ac.ir/login')
-
-
-driver.find_element('id',"username").send_keys(username)
-
-driver.find_element('id',"password").send_keys(password)
-
-
-
-driver.find_element("css selector", ".loginButton").click()
-
-driver.quit()
-
-
+l.save_file()
 
 
 
